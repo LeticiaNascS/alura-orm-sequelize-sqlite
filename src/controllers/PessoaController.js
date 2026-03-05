7a2baeeae1c3de6d04308a48efe08e8c742150c1
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-03-05 14:50:39 -0300

    Criando rota e controller

---
+++ src/controllers/PessoaController.js
@@ -0,0 +1,13 @@
+class PessoaController {
+  static async pegaTodas (req, res) {
+    try{
+      //acessar o model
+    } catch(erro) {
+      //erro
+    }
+
+  }
+}
+
+module.exports = PessoaController;
+
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const pessoas = require('./pessoasRoutes.js'); 
+
+
+module.exports = app => {
+  app.use(
+    express.json(),
+    pessoas
+
+  );
+};
