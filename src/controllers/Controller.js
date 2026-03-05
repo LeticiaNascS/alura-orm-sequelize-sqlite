cf3c898627bb0f11971c3f77380860acdc1c384d
Leticia Nascimento <let.lu.nascimento@gmail.com>
2026-03-05 18:31:58 -0300

    Crud em andamento

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,17 +1,32 @@
-
 class Controller {
-  constructor(entidadeService){
+  constructor(entidadeService) {
     this.entidadeService = entidadeService;
   }
 
-  async pegaTodos(req,res){
-    try{
-      const listaDeRegistros = await this.entidadeService.pegaTodosOsRegistros();
+  async pegaTodos(req, res) {
+    try {
+      const listaDeRegistros =
+        await this.entidadeService.pegaTodosOsRegistros();
       return res.status(200).json(listaDeRegistros);
+    } catch (erro) {
+      return res.status(500).send('Erro ao listar pessoas');
+    }
+  }
 
-    }catch (erro) {
-      console.log(erro);
-      return res.status(500).send( 'Erro ao listar pessoas');
+  async atualiza(req,res){
+   
+    const { id } = req.params;
+    const dadosAtualizados = req.body;    
+    
+    try{
+      const foiAtualizado = await this.entidadeService.atualizaRegistro(dadosAtualizados, Number (id));
+
+      if(!foiAtualizado){
+        return res.status(400).json({mensagem:'Registro não foi atualizado'});
+      }
+      return res.status(200).json({mensagem: 'Registro atualizado com sucesso'});
+    } catch (erro) {
+      return res.status(500).send('Erro ao tentar atualizar os dados');
     }
   }
 }
